943c386ae90094d983c70c14c6a0abcc65cb7102
Michael-A-McMahon <Michael.A.McMahon@oracle.com>
2022-05-21 10:52:31 -0700

    Correct -R option

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -52,7 +52,7 @@
 startUp=$PWD/$1/startup
 mkdir -p $startUp
 cp $PWD/startup/* $startUp
-chmod -r u+rw $startUp
+chmod -R u+rw $startUp
 
 # Create the 99_ready.sh script. It will touch a file in the mounted startup
 # directory.
