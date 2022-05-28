c78c95dbc7f26361c4a3d8e8712ccefec4b3c5a9
Michael-A-McMahon <Michael.A.McMahon@oracle.com>
2022-05-28 12:08:45 -0700

    Use -p with a dynamic range

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -62,10 +62,10 @@ cd docker-images/OracleDatabase/SingleInstance/dockerfiles/
 # The startup directory is mounted. It contains a createUser.sql script that
 # creates a test user. The docker container will run this script once the
 # database has started.
-# The -P option has the the database port number, 1521, mapped to an available
-# port on the host system.
+# The -p option has the database port number, 1521, mapped to any available
+# port in the range between 49152 and 65535
 containerName=test_db_$1
-docker run --name $containerName --detach --rm -P -v $startUp:/opt/oracle/scripts/startup oracle/database:$1-xe
+docker run --name $containerName --detach --rm -p 49152–65535:1521 -v $startUp:/opt/oracle/scripts/startup oracle/database:$1-xe
 
 # Wait for the database instance to start. The final startup script will create
 # a file named "ready" in the startup scripts directory. When that file exists, 
